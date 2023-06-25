56b105b86b80fe1f9ab54a7d23c399f9a0c7429b
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-25 12:56:04 -0500

    Event Binding

--- Proyecto/proyecto-nuevo/src/app/personas/personas.component.ts
+++ Proyecto/proyecto-nuevo/src/app/personas/personas.component.ts
@@ -3,13 +3,19 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-personas',
     templateUrl: './personas.component.html',
-    //styleUrls: ['./personas.component.css']
-    styles: [`
-        h1{
-            color: blue;
-        }
-    `]
+    styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent{
     desabilitar = false;
+    mensaje = "No se ha agregado Ninguna Persona";
+    titulo = "";
+
+    agregarPersona(){
+        this.mensaje = "Persona Agregada"
+    }
+
+    modificarTitulo(event: Event){
+        console.log("Entrando a Modificar Titulo")
+        this.titulo = (<HTMLInputElement>event.target).value;
+    }
 }
