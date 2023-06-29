3aea6ce563b962117a3e0ec7fb943c09c24cb4e9
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-06-29 11:42:34 -0500

    Uso de ngFor

---
+++ Proyecto/listar-personas/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Persona } from './persona.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'listado de Personas';
+  personas: Persona[] = [new Persona("Julian","Carrillo"), new Persona("Juan","Perez"), new Persona("Karla","Lara")]
+}
