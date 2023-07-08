a0daabfd1e37aac12cb86959c5460ffb5ba08a70
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-07-07 21:24:19 -0500

    Navegacion entre componentes en Angular

--- Proyecto/listar-personas/src/app/personas/formulario/formulario.component.ts
+++ Proyecto/listar-personas/src/app/personas/formulario/formulario.component.ts
@@ -1,32 +1,46 @@
-import { Component, ElementRef} from '@angular/core';
+import { Component, ElementRef, OnInit} from '@angular/core';
 import { Persona } from '../../persona.model';
 import { LoggingService } from '../../LoggingService.service';
 import { PersonasService } from '../../personas.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
 
   nombreInput: string;
   apellidoInput: string;
-
+  index: number;
 
   constructor(private loggingService: LoggingService,
     private personaService: PersonasService,
-    private router: Router){
+    private router: Router,
+    private route: ActivatedRoute){
       this.personaService.saludar.subscribe(
         (indice: number) => alert("El indice es: " + (indice+1))
       );
   }
 
+  ngOnInit() {
+    this.index = this.route.snapshot.params['id'];
+    if(this.index){
+      let persona: Persona = this.personaService.encontrarPersona(this.index);
+      this.nombreInput = persona.nombre;
+      this.apellidoInput = persona.apellido;
+    }
+  }
+
   onGuardarPersona(){
     let persona1 = new Persona(this.nombreInput,this.apellidoInput);
+    if(this.index){
+      this.personaService.modificarPersonas(this.index,persona1);
+    }else{
     this.personaService.agregarPersona(persona1);
-    this.router.navigate(['personas']);
+  }
+  this.router.navigate(['personas']);
   }
 
 }
