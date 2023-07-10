185d933aea50eb026f0b718485f2eb662341b723
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-07-10 11:46:17 -0500

    Aplicación de Query Params

--- Proyecto/listar-personas/src/app/personas/formulario/formulario.component.ts
+++ Proyecto/listar-personas/src/app/personas/formulario/formulario.component.ts
@@ -14,6 +14,7 @@ export class FormularioComponent implements OnInit {
   nombreInput: string;
   apellidoInput: string;
   index: number;
+  modoEdicion: number;
 
   constructor(private loggingService: LoggingService,
     private personaService: PersonasService,
@@ -26,7 +27,8 @@ export class FormularioComponent implements OnInit {
 
   ngOnInit() {
     this.index = this.route.snapshot.params['id'];
-    if(this.index){
+    this.modoEdicion = +this.route.snapshot.queryParams['modoEdicion'];
+    if(this.modoEdicion != null && this.modoEdicion ===1 ){
       let persona: Persona = this.personaService.encontrarPersona(this.index);
       this.nombreInput = persona.nombre;
       this.apellidoInput = persona.apellido;
@@ -35,7 +37,7 @@ export class FormularioComponent implements OnInit {
 
   onGuardarPersona(){
     let persona1 = new Persona(this.nombreInput,this.apellidoInput);
-    if(this.index){
+    if(this.modoEdicion != null && this.modoEdicion ===1){
       this.personaService.modificarPersonas(this.index,persona1);
     }else{
     this.personaService.agregarPersona(persona1);
