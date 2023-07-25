cfc15659310a9c2c029f07df3236fcc479fdfaa8
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-07-24 20:56:48 -0500

    Creacion de diferentes componentes del Proyecto

--- Proyecto/control-clientes/src/app/app.component.ts
+++ Proyecto/control-clientes/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'control-clientes';
+
+}
+
+export const enviroments = {
+    production: false;
+    firestore:{
+      apiKey: "AIzaSyChT0uTORDxiw_4VIqeJl3AHh3bnGXBdxU",
+    authDomain: "control-clientes-c0162.firebaseapp.com",
+    projectId: "control-clientes-c0162",
+    storageBucket: "control-clientes-c0162.appspot.com",
+    messagingSenderId: "13249087232"
+    }
 }
--- Proyecto/control-clientes/src/app/app.module.ts
+++ Proyecto/control-clientes/src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { enviroments } from './app.component';
+import {AngularFireModule } from '@angular/fire/compat';
+import {AngularFirestoreModule,Settings} from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(enviroments.firestore, 'control-clientes'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    FlashMessagesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
